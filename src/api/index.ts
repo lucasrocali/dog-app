05bfb97c6a456d4a5c67a7d98425133570386b8d
Lucas Rocali <rocali@outlook.com>
2021-03-19 12:53:10 -0300

    feat: handle get images

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,17 +1,17 @@
 import axios from 'axios'
 
 const dogBreedsApi = axios.create({
-  baseURL: 'https://dog.ceo/api/breeds',
+  baseURL: 'https://dog.ceo/api',
 });
 
 export async function getAllBreeds(): Promise<string[]> {
-  const response = await dogBreedsApi.get('/list/all')
+  const response = await dogBreedsApi.get('/breeds/list/all')
   const dataMessage: { [key: string]: string[] } = response.data.message
   return Object.keys(dataMessage)
 }
 
-export async function getBreedImages(breed: string): Promise<string[]> {
-  const response = await dogBreedsApi.get(`/${breed}/images/random/3`)
+export async function getBreedImages(breed: string, size: number = 3): Promise<string[]> {
+  const response = await dogBreedsApi.get(`breed/${breed}/images/random/${size}`)
   const images: string[] = response.data.message
   return images
 }
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,35 +2,69 @@ import React, { useEffect, useState } from 'react';
 import { SafeAreaView, StyleSheet, Text, TextInput, View, FlatList } from 'react-native';
 import { getAllBreeds, getBreedImages } from 'src/api'
 
+type Dog = {
+  breed: string
+  images: string[]
+}
+
 export default function Home() {
 
   const [searchingText, setSearchingText] = useState<string>('')
   const [allBreeds, setAllBreeds] = useState<string[]>([])
   const [filteredBreeds, setFilteredBreeds] = useState<string[]>([])
-  const [imagesByBreed, setImagesByBreed] = useState<{ [key: string]: string }>({})
+  const [imagesByBreed, setImagesByBreed] = useState<{ [key: string]: string[] }>({})
 
-  async function getBreeds() {
+  async function handleGetBreeds() {
     const breeds = await getAllBreeds()
     setAllBreeds(breeds)
   }
 
+  async function handleGetBreedImages(breed: string) {
+    const images = await getBreedImages(breed)
+    setImagesByBreed(imagesByBreed => ({
+      ...imagesByBreed,
+      [breed]: images
+    }))
+  }
+
+  function handleBreedImages(breed: string) {
+    if (!imagesByBreed[breed]) {
+      handleGetBreedImages(breed)
+    }
+  }
+
   useEffect(() => {
-    getBreeds()
+    handleGetBreeds()
   }, [])
 
   useEffect(() => {
-    const filtered = allBreeds.filter((breed) => breed.toLocaleLowerCase().startsWith(searchingText.toLocaleLowerCase()))
-    setFilteredBreeds(filtered)
+    if (searchingText.length > 0) {
+      const filtered = allBreeds.filter((breed) => breed.toLocaleLowerCase().startsWith(searchingText.toLocaleLowerCase()))
+      setFilteredBreeds(filtered)
+    } else {
+      setFilteredBreeds([])
+    }
   }, [searchingText])
 
-  const renderItem = ({ item: breed }: { item: string }) => (
-    <Text>{breed}</Text>
+  useEffect(() => {
+    filteredBreeds.forEach(breed => handleBreedImages(breed))
+  }, [filteredBreeds])
+
+  const renderItem = ({ item: { breed, images } }: { item: Dog }) => (
+    <View style={styles.row}>
+      <Text key={breed}>{breed}</Text>
+      {images && images.map(image => (
+        <Text key={image}>{image}</Text>
+      ))}
+    </View>
   );
+
+  const dogs: Dog[] = filteredBreeds.map(breed => ({ breed, images: imagesByBreed[breed] }))
 
   return (
     <SafeAreaView style={styles.container}>
       <FlatList
-        data={filteredBreeds}
+        data={dogs}
         ListHeaderComponent={(
           <View style={styles.header}>
             <TextInput
@@ -57,5 +91,8 @@ const styles = StyleSheet.create({
   textInput: {
     borderColor: '#DDD',
     borderWidth: 1
+  },
+  row: {
+
   }
 });
